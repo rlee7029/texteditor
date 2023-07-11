6b02c2fdc4a2b8da714e7a63dd81d0a877dfb53f
Robert Lee <robertlee@roberts-mbp.lan>
2023-07-11 00:22:57 -0700

    finsihed TODO for webpack, src-sw

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
 
-    module: {
-      rules: [
-        
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'J.A.T.E.',
+    }),
+
+    new InjectManifest ({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+    // Creates a manifest.json file.
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'J.A.T.E.',
+      description: 'Take Notes with JavaScript Syntax highlighting',
+      background_color: '#225ca3',
+      theme_color: '#225ca3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
       ],
-    },
-  };
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
+      },     
+    ],
+  },
+};
 };
